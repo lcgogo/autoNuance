cc7e5166d5f30d91219a9fd309a18a1c51d1204d
lcgogo <lcgogo123@163.com>
2017-05-26 08:00:10 +0000

    a tool to config lcgogo123@163.com github

---
+++ gitconfig.sh
@@ -0,0 +1,24 @@
+git config --global user.name "lcgogo"
+git config --global user.email "lcgogo123@163.com"
+
+#################################################
+# function ReplaceSpecialString
+# $1 is the full path file name
+# $2 is the keyword of the line whhich contains the string you want to replace
+# $3 is the string you want to replace from
+# $4 is the string you want to replace to
+function ReplaceSpecialString(){
+if [ ! -f $1 ];then
+  echo $1 does not exist. Pls check Nuance is installed correctly. Exit now.
+  exit 1
+fi
+
+lineNum=`cat -n $1 | grep "$2" | awk '{print $1}'`
+echo  Replace $3 to $4 in file $1 line $lineNum.
+
+sedString="$lineNum""s/""$3""/""$4""/g"
+
+sed -i "$sedString" $1
+}
+
+ReplaceSpecialString ".git/config" "url" "github.com" "lcgogo@github.com" 
